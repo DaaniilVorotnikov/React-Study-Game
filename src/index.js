dd38e77aeb6a3a2fc61b73f71353eb4465ca2e92
Даниил <daniil@MacBook-Pro-Daniil.local>
2021-07-27 07:34:46 +0300

    new game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Board from './Board';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Square from './Square';
+import Game from './Game';
+
+<Square />;
+<Board />;
+<Game />;
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
